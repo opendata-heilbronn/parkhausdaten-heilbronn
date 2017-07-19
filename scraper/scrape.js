aa30df6116791cd22f55ce48480719e3f6f5772a
Leandro Späth <leandro.spaeth@gmail.com>
2017-07-20 00:29:28 +0200

    add commented out array structure

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -1,16 +1,18 @@
 ﻿const cheerio = require('cheerio');
 
 function scrape(html) {
-	result = {carparks: {}}
+	let result = {carparks: {}};
+	//let result = {carparks: []};
 	let $ = cheerio.load(html);
 	let time = $('div.carparkHead > div').text();
-	time = time.replace("Datum: ", "").replace(" - Uhrzeit: ", " ");
+	time = time.replace("Datum: ", "").replace(" - Uhrzeit: ", " "); 
 	result.time = time;
     $('div.carparkContent').each(function(i, element){
 	  let location = $(this).find('.carparkLocation > a').text();
 	  let freeString = $(this).children('.col-xs-5').text();
 	  let free = freeString.replace("Freie Parkplätze: ", "") || "-1";
 	  result.carparks[location] = {free: +free};
+	  //result.carparks.push({name: location, free: +free});
     });
 	return result;
 }
