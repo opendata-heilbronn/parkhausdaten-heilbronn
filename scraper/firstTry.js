c6d758e684164bd47dea77ca4d8f261c081a5c03
Leandro Späth <leandro.spaeth@gmail.com>
2017-07-20 00:05:44 +0200

    add first try

---
+++ scraper/firstTry.js
@@ -0,0 +1,22 @@
+const request = require('request');
+const cheerio = require('cheerio');
+
+const apiUrl = "https://www.heilbronn.de/allgemeine-inhalte/ajax-parkhausbelegung.html?type=1496993343";
+
+let first = true;
+
+request(apiUrl,(error, response, html) => {
+  if (!error && response.statusCode == 200) {
+    //console.log(html);
+	var $ = cheerio.load(html);
+	console.log($('div.carparkHead > div').text());
+    $('div.carparkContent').each(function(i, element){
+      //var a = $(this).prev();
+      //console.log(a.text());
+		  first = false;
+		  console.log($(this).find('.carparkLocation > a').text());
+		  console.log($(this).children('.col-xs-5').text());
+		  console.log();
+    });
+  }
+});
