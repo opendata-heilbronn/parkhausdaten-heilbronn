9329aa0142060e832dc2c24fded23956e3983341
Leandro Späth <leandro.spaeth@gmail.com>
2017-07-23 03:20:58 +0200

    finish scraper (including posting to db)

--- scraper/scrape.js
+++ scraper/scrape.js
@@ -4,22 +4,22 @@ const moment = require('moment');
 function scrape(html) {
     let result = {carparks: {}};
     //let result = {carparks: []};
-    
+
     let $ = cheerio.load(html);
-    
+
     let updateTime = $('div.carparkHead > div').text();                     //extract update time
     updateTime = updateTime
         .replace("Datum: ", "")
         .replace(" - Uhrzeit: ", " ");                                      //quite dirty method, but should work
     let t = moment(updateTime, 'DD.MM.YYYY HH:mm');                         //parse update time
     result.time = t.unix();
-    
-    $('div.carparkContent').each(function(i, element){                      //loop thorugh all carpark elements
-      let location = $(this).find('.carparkLocation > a').text();           //extract carpark name
-      let freeString = $(this).children('.col-xs-5').text();                //extract carpark free places
-      let free = freeString.replace("Freie Parkplätze: ", "") || undefined; //remove unnecessary string, write undefined instead of empty string
-      result.carparks[location] = {free: +free};                            //add carpark to results
-      //result.carparks.push({name: location, free: +free});
+
+    $('div.carparkContent').each(function (i, element) {                      //loop thorugh all carpark elements
+        let location = $(this).find('.carparkLocation > a').text();           //extract carpark name
+        let freeString = $(this).children('.col-xs-5').text();                //extract carpark free places
+        let free = freeString.replace("Freie Parkplätze: ", "") || -1;        //remove unnecessary string, write undefined instead of empty string
+        result.carparks[location] = {free: +free};                            //add carpark to results
+        //result.carparks.push({name: location, free: +free});
     });
     return result;
 }
--- scraper/scraper.js
+++ scraper/scraper.js
@@ -4,21 +4,21 @@ const scr = require('./scrape');
 const apiUrl = "http://www.heilbronn.de/allgemeine-inhalte/ajax-parkhausbelegung.html?type=1496993343";
 const localApiUrl = "http://localhost:3001";
 
-const internalIds = {
-    'Am Bollwerksturm':             'hn-cp-bollwerksturm',
-    'City-Parkhaus Experimenta':    'hn-cp-experimenta',
-    'Harmonie':                     'hn-harmonie',
-    'Käthchenhof':                  'hn-kaethchenhof',
-    'Kiliansplatz':                 'hn-kiliansplatz',
-    'Stadtgalerie':                 'hn-stadtgalerie',
-    'Theaterforum K3':              'hn-k3',
-    'Wollhaus':                     'hn-wollhaus'
+const internalIds = { //relation between names from heilbronn api to our api
+    'Am Bollwerksturm': 'hn-cp-bollwerksturm',
+    'City-Parkhaus Experimenta': 'hn-cp-experimenta',
+    'Harmonie': 'hn-harmonie',
+    'Käthchenhof': 'hn-kaethchenhof',
+    'Kiliansplatz': 'hn-kiliansplatz',
+    'Stadtgalerie': 'hn-stadtgalerie',
+    'Theaterforum K3': 'hn-k3',
+    'Wollhaus': 'hn-wollhaus'
 };
 
-function aRequest(url) {
+function aRequest(url) { //truly asynchronous request
     return new Promise((resolve, reject) => {
         request(url, (err, res, body) => {
-            if(!err /*&& res.statusCode == 200*/)
+            if (!err /*&& res.statusCode == 200*/)
                 resolve(body);
             else {
                 reject(err);
@@ -27,24 +27,54 @@ function aRequest(url) {
     });
 }
 
-async function getMongoId(name) {
+async function getMongoId(name) { //return mongodb id
     let intId = internalIds[name];
-    if(intId === undefined)
+    if (intId === undefined)
         console.error("Did the data format change? Could not find", name, "in data");
     else {
-        console.log(localApiUrl + '/carpark?internalId=' + intId);
-        let res = await aRequest('http://127.0.0.1:3001/carpark?internalId=' + intId);
-        console.log(res);
+        let res;
+        try {
+            res = await aRequest(localApiUrl + '/carpark?internalId=' + intId);
+        }
+        catch (e) {
+            console.error("could not establish connection to db-api, check if it's running on the correct port");
+            return undefined;
+        }
         let carPark = JSON.parse(res);
-        console.log(carPark);
-        return carPark._id;
+        if (carPark.status == 'error') {
+            console.error("error getting data from database", carPark.error || "");
+            return undefined;
+        }
+        return carPark[0]._id;
     }
 }
 
-async function doPoll() {
+async function postData(carParkId, timestamp, free) { //post final data to influxDB
+    let opts = {
+        url: localApiUrl + '/carParkStatus/' + carParkId,
+        method: 'PUT',
+        json: {timestamp: timestamp, freeCarPorts: free}
+    };
+    await request(opts, (err, res, body) => {
+        if (err || res.statusCode != 200 || body.status != "success") {
+            console.error('error posting carPark data', err || "", body);
+        }
+    })
+}
+
+async function doPoll() { //main function
     let res = await aRequest(apiUrl);
-    console.log(scr.scrape(res));
-    getMongoId("Wollhaus");
+    res = scr.scrape(res);
+    let msg = {};
+    for (let name of Object.keys(res.carparks)) {
+        let carpark = res.carparks[name];
+        if (carpark.free > -1)
+            msg[name] = carpark.free;
+        let id = await getMongoId(name);
+        if (carpark.free > -1 && id) //id returns undefined if error occurs
+            await postData(id, res.time, carpark.free);
+    }
+    console.log(res.time, JSON.stringify(msg));
 }
 
 doPoll();
