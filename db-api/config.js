87593f1f32aac47f5e013fcc714bf130334d6522
LeoDJ <leandro.spaeth@gmail.com>
2018-06-28 19:24:29 +0200

    add influx auth

--- db-api/config.js
+++ db-api/config.js
@@ -4,6 +4,8 @@ module.exports = {
         port: process.env.INFLUX_PORT || 8086,
         protocol: process.env.INFLUX_PROTOCOL || 'https',
         database: process.env.INFLUX_DB ||'carpark',
+        username: process.env.INFLUX_USER,
+        password: process.env.INFLUX_PASSWORD
     },
     mongodb: {
         url: process.env.MONGODB_URL || 'mongodb://localhost:27017/carpark'
