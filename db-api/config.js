39b365651dd3b5cad6376b6d558cc8287ec51f2f
LeoDJ <leandro.spaeth@gmail.com>
2018-06-28 16:53:46 +0200

    add docker foo

--- db-api/config.js
+++ db-api/config.js
@@ -1,12 +1,12 @@
 module.exports = {
     influx: {
-        host: 'localhost',
-        database: 'carpark',
+        host: process.env.INFLUX_HOST || 'localhost',
+        database: process.env.INFLUX_DB ||'carpark',
     },
     mongodb: {
-        url: 'mongodb://localhost:27017/carpark'
+        url: process.env.MONGODB_URL || 'mongodb://localhost:27017/carpark'
     },
     api: {
-        port: 3000
+        port: process.env.API_PORT || 3001
     }
 };
